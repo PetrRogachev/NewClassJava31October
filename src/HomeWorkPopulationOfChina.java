ccd1b55b98363a48d593a4854907d0fd0b8cd672
par55 <par555@mail.ru>
2022-11-07 11:58:01 +0100

    przyklad

---
+++ src/HomeWorkPopulationOfChina.java
@@ -0,0 +1,18 @@
+public class HomeWorkPopulationOfChina {
+    public static void main(String[] args) {
+        long populationOfChina = 1_411_778_724;
+        int numbersOfworldCountries = 197;
+        long fictionalWorldsPopulation = populationOfChina * numbersOfworldCountries;
+        System.out.println(fictionalWorldsPopulation);
+
+        long FeliksSecondsLife = 3_011_456_567l;
+        int secondsIn1hour = 60 * 60;
+        int secondsIn1day = secondsIn1hour * 24;
+        long secondsIn1year = secondsIn1day * 365;
+        long FeliksYearsOfLife = FeliksSecondsLife / secondsIn1year;
+        System.out.println(FeliksYearsOfLife);
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        char s; // 2 bayta
+
+        char symbol = 'a' + 1001;
+        System.out.println(symbol);
+
+    }
+
+
+
+
+
+}
